86c26adcf3369c1f9cd12d01582eb4a661c78641
Matt Drapkin <matt@drapkin.org>
2022-11-06 12:27:18 -0500

    Completed basic functionality for H4I joke project

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 console.log("Welcome to the Lobster Club Improv Comedy Show!");
-const btn = null;
-console.log("Button?: ", btn);
+const randomBtn = document.getElementById('random-btn');
+const hipsterBtn = document.getElementById('hipster-btn');
+const historyBtn = document.getElementById('history-btn');
+const beesBtn = document.getElementById('bees-btn');
 
-function getJoke() {
-    console.log("ur mom");
+
+const joke = document.getElementById('joke')
+const options = { 
+    method: 'GET', // specify this is a GET request, not a PUT or POST
+    headers: {
+      "Accept" : "application/json" // request the response in JSON format
+    }
+  }
+
+randomBtn.addEventListener("click", () => getJoke());
+hipsterBtn.addEventListener("click", () => getJoke('hipster'));
+historyBtn.addEventListener("click", () => getJoke('history'));
+beesBtn.addEventListener("click", () => getJoke('bees'));
+
+const getJoke = (category) => {
+    const url = category ? 
+    `https://icanhazdadjoke.com/search?term=${category}` :
+    'https://icanhazdadjoke.com/'
+    fetch(url, options)
+	.then(response => {
+		return response.json()
+	})
+	.then(responseAsJson => {
+		joke.innerHTML = category ? 
+        responseAsJson.results[0].joke :
+        responseAsJson.joke
+	})
+	.catch(error => {
+		console.error(error)
+	})
 }
-
-getJoke();
